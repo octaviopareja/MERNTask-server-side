d50e25f54de111500d0bca5e63b495cec9e89ad3
Octavio Pareja <octaviopareja05@gmail.com>
2021-04-06 16:07:12 -0300

    .
    .

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.json({ extented: true }));
 
 //puerto para el servidor
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 //importar rutas
 app.use("/api/usuarios", require("./routes/usuarios"));
@@ -29,6 +29,6 @@ app.use("/api/proyectos", require("./routes/proyectos"));
 app.use("/api/tareas", require("./routes/tareas"));
 
 //arrancar la app
-app.listen(PORT, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 });
